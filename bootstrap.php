12ce491d35acd7040a68e4b277ba7e5574999a81
MorelSami <mksami237@gmail.com>
2024-05-08 00:59:22 -0400

    test setup

--- bootstrap.php
+++ bootstrap.php
@@ -1,2 +1,25 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
+require_once  vendor/autoload.php;
+
+//load environmental variable file [.env]
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+define('ENVIRONMENT', $_ENV['ENVIRONMENT']);
+
+switch (ENVIRONMENT)
+{
+	case 'development':
+		error_reporting(-1);
+		ini_set('display_errors', 1);
+	break;
+
+	case 'testing':
+	case 'production':
+		ini_set('display_errors', 0);
+	break;
+
+	default:
+		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+		echo 'The application environment is not set correctly.';
+		exit(1); // EXIT_ERROR
+}
