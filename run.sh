563b8f30074734f0e150524191857b331e9931d9
MorelSami <mksami237@gmail.com>
2024-05-10 22:59:03 -0400

    Front & Back end update

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ docker run -d -p 8086:8086 -e APP_NAME=Rukbe-Image-Uploader -e APP_ENV=developme
 #echo 'Run Rubke Uploader (front-end) ...'
 docker build -t rukbe/front img-uploader/
 #docker run -d -p 3000:3000 -v /home/msami/workspace/RukbeChallenge/img-uploader:/src --restart=always --name=rukbe-front rukbe/front:latest 
-docker run -d -p 3000:3000 --restart=always --name=rukbe-front rukbe/front:latest
+docker run -d -p 3000:3000 -e UPLOADER_API = http://localhost:8086/server.php --restart=always --name=rukbe-front rukbe/front:latest
